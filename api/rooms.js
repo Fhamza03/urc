b1367639b1db3ec11328de6e6edf435dab33b688
Hamza FELLAH <fellahhamza48@gmail.com>
2025-11-10 17:36:29 +0100

    fixing rooms

--- api/rooms.js
+++ api/rooms.js
@@ -1,6 +1,10 @@
 import { getConnecterUser } from "../lib/session.js";
 import { sql } from "@vercel/postgres";
 
+export const config = {
+  runtime: 'edge',
+};
+
 export default async function handler(request, response) {
   try {
     const user = await getConnecterUser(request);
